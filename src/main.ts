2bedce18fd584d9f69722faa4446239b043fd604
Jared <jaredtang2024@outlook.com>
2024-08-25 19:45:34 +0800

    配置src别名

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 //vue3 create app 可以用来创建应用实例方法
 import { createApp } from 'vue'
 //引入根组件App
-import App from './App.vue'
+import App from '@/App.vue'
 //利用createApp方法创建应用实例，且将应用挂载到挂载点上
 createApp(App).mount('#app')
 const app = createApp(App);
