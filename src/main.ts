0d7e0172e9893fbafb21072137704a834ff3f087
Jared <jaredtang2024@outlook.com>
2024-08-25 20:04:29 +0800

    引入重置央视

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 //vue3 create app 可以用来创建应用实例方法
 import { createApp } from 'vue'
+//引入清楚默认样式
+import '@/style/reset.scss'
 //引入根组件App
-import App from '@/App.vue'
+import App from './App.vue'
 //利用createApp方法创建应用实例，且将应用挂载到挂载点上
 createApp(App).mount('#app')
 const app = createApp(App);
